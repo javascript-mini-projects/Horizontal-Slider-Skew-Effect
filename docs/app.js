b8c84ededd31e59ca2ac3b12733ff28488e80fac
Harpreet Singh <designsbyharp@gmail.com>
2021-04-27 18:44:32 -0400

    JavaScript completed
    created skew effect on window scroll

--- docs/app.js
+++ docs/app.js
@@ -1,5 +1,30 @@
 let horizontalScroller = function () {
   // DOM element(s)
+  const slider = document.querySelector(".slider");
+
+  // when windowY scrolls move the slider
+  window.onscroll = () => {
+    slider.style.left = `${-window.scrollY}px`;
+  };
+
+  // Variables to work with the scroll
+  let currentPos = slider.getBoundingClientRect().left;
+  let newPos, diff, speed;
+
+  // get the new position and check difference with current and create skew effect
+  const callDistort = () => {
+    newPos = slider.getBoundingClientRect().left;
+    diff = newPos - currentPos;
+    speed = diff * 0.25;
+
+    slider.style.transform = `skewX(${speed}deg)`;
+
+    currentPos = newPos;
+
+    requestAnimationFrame(callDistort);
+  };
+
+  callDistort();
 };
 
 // Invoke code
